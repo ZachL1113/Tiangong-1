b392d34a09936f3891d0f5bc605b35c10b149cc8
ZachL1113 <E1481840@u.nus.edu>
2025-06-24 11:25:12 +0800

    Delete src directory

--- src/main/java/com/example/demo/game/service/dto/MoveRequest.java
+++
@@ -1,3 +0,0 @@
-package com.example.demo.game.service.dto;
-
-public record MoveRequest(String boardJson, String direction, Integer pieceId) {}
--- src/main/java/com/example/demo/level/LevelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.level;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LevelRepository extends JpaRepository<Level, Long> {
-    Level findByLevelIndex(int levelIndex);
-    List<Level> findAllByOrderByLevelIndexAsc();
-}
