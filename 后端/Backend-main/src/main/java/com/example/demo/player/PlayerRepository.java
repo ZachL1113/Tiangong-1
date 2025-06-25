77c687bcb791feabaeaabfcb9e4ad8340d4351ee
huyunxin <huyunxin060407@gmail.com>
2025-06-25 17:33:32 +0800

    delete overlapping backend code

--- 后端/Backend-main/src/main/java/com/example/demo/player/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.player;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-    Player findByName(String name);
-}
