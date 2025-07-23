b826f59bbf11b7d45c27d2735b41887c2aa7ec44
ZachL1113 <E1481840@u.nus.edu>
2025-07-23 14:05:20 -0700

    Update PlayerRepository.java

--- backend/src/main/java/com/example/demo/player/PlayerRepository.java
+++ backend/src/main/java/com/example/demo/player/PlayerRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     Player findByName(String name);
+    Player findByProviderAndProviderId(String provider, String providerId);
 }
